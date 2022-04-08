78bbbe6ff8e96e490e5252fa3e766fbe358594ee
Collene De Silva <collene.desilva@gmail.com>
2022-04-09 00:11:40 +0800

    Add archive, promote, and delete button

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,73 +1,241 @@
 import { Box, Card, Typography, Grid } from "@mui/material";
+import { useState, useEffect } from "react";
 import CardTypography from "./CardTypography";
 import CommonButton from "./CommonButton";
+import Swal from "sweetalert2";
 
 export default function ProductCard() {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch(`${ process.env.REACT_APP_API_URL }/products/`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            setProducts(data)
+        })
+    });
+
+    function deleteProduct(product) {
+        
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "Product will be deleted and cannot be retrieved!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#990f02',
+            cancelButtonColor: '#4b5320',
+            confirmButtonText: 'Yes, delete!'
+            }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`${ process.env.REACT_APP_API_URL }/products/${product._id}`,{
+                    method: 'DELETE',
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem('token')}`
+                    },
+                })
+                Swal.fire(
+                'All set!',
+                'Order deleted!',
+                'success'
+                )
+            }
+            })
+    }
+
+    function toggleActive(product) {
+        if (!(product.isActive)) {
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "Product will be active after this",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#4b5320',
+                cancelButtonColor: '#990f02',
+                confirmButtonText: 'Yes, activate!'
+              }).then((result) => {
+                if (result.isConfirmed) {
+                    fetch(`${ process.env.REACT_APP_API_URL }/products/${product._id}/archive`,{
+                        method: 'PUT',
+                        headers: {
+                            Authorization: `Bearer ${localStorage.getItem('token')}`
+                        },
+                    })
+                  Swal.fire(
+                    'All set!',
+                    'Product is now active!',
+                    'success'
+                  )
+                }
+              })
+        } else {
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "Product will be archived after this",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#4b5320',
+                cancelButtonColor: '#990f02',
+                confirmButtonText: 'Yes, archive!'
+              }).then((result) => {
+                if (result.isConfirmed) {
+                    fetch(`${ process.env.REACT_APP_API_URL }/products/${product._id}/archive`,{
+                        method: 'PUT',
+                        headers: {
+                            Authorization: `Bearer ${localStorage.getItem('token')}`
+                        },
+                    })
+                  Swal.fire(
+                    'All set!',
+                    'Product is now archived!',
+                    'success'
+                  )
+                }
+              })
+        }
+    }
+
+    function togglePromote(product) {
+        if (!(product.isPromoted)) {
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "Product will be promoted after this",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#4b5320',
+                cancelButtonColor: '#990f02',
+                confirmButtonText: 'Yes, promote!'
+              }).then((result) => {
+                if (result.isConfirmed) {
+                    fetch(`${ process.env.REACT_APP_API_URL }/products/${product._id}/promote`,{
+                        method: 'PUT',
+                        headers: {
+                            Authorization: `Bearer ${localStorage.getItem('token')}`
+                        },
+                    })
+                  Swal.fire(
+                    'All set!',
+                    'Product is now promoted!',
+                    'success'
+                  )
+                }
+              })
+        } else {
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "Product will not be promoted after this",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#4b5320',
+                cancelButtonColor: '#990f02',
+                confirmButtonText: 'Yes, undo promote!'
+              }).then((result) => {
+                if (result.isConfirmed) {
+                    fetch(`${ process.env.REACT_APP_API_URL }/products/${product._id}/promote`,{
+                        method: 'PUT',
+                        headers: {
+                            Authorization: `Bearer ${localStorage.getItem('token')}`
+                        },
+                    })
+                  Swal.fire(
+                    'All set!',
+                    'Undo promotion!',
+                    'success'
+                  )
+                }
+              })
+        }
+    }
+
     return (
-        <Grid
-            item
-            xs={12}
-            key="12321321"
-        >
-            <Card variant="outlined" sx={{ padding: 2 }}>
-                <Grid container spacing={4} justifyContent="center">
-                    <Grid item alignSelf="center" xs={12} sm={6} md={2} lg={2}>
-                        <Box
-                            sx={{
-                                display: "flex",
-                                flexDirection: "column",
-                            }}
-                        >
-                            <Box sx={{mb: 2}}>
-                            <img src="http://placekitten.com/200/200" alt="trip lang" style={{width: "100%"}}/>
-                            <Typography variant="subtitle1">Product View</Typography>
-                            </Box>
-                            <Box>
-                            <img src="http://placekitten.com/200/200" alt="trip lang" style={{width: "100%"}}/>
-                            <Typography variant="subtitle1">Room View</Typography>
-                            </Box>
-                        </Box>
+        <>
+       
+            {
+                products.map(product => {
+                    return (
+                    <Grid
+                        item
+                        xs={12}
+                        key={product._id}
+                    >
+                        <Card variant="outlined" sx={{ padding: 2 }}>
+                            <Grid container spacing={4} justifyContent="center">
+                                <Grid item alignSelf="center" xs={12} sm={6} md={2} lg={2}>
+                                    <Box
+                                        sx={{
+                                            display: "flex",
+                                            flexDirection: "column",
+                                        }}
+                                    >
+                                        <Box sx={{mb: 2}}>
+                                        <img src={product.imageURL} alt="product view" style={{width: "100%"}}/>
+                                        <Typography variant="subtitle1">Product View</Typography>
+                                        </Box>
+                                        <Box>
+                                        <img src={product.imageRoomURL} alt="room view" style={{width: "100%"}}/>
+                                        <Typography variant="subtitle1">Room View</Typography>
+                                        </Box>
+                                    </Box>
+                                </Grid>
+                                
+                                <Grid item xs={12} sm={6} md={6} lg={6}>
+                                    <Box
+                                        sx={{
+                                            display: "flex",
+                                            flexDirection: "column",
+                                            justifyContent: "center",
+                                            alignItems: "center"
+                                        }}
+                                    >
+                                        <CardTypography props={{title: "Product Id:", content: product._id}} />
+                                        <CardTypography props={{title: "Name:", content: product.name}} />
+                                        <CardTypography props={{title: "Description:", content: product.description}} />
+                                        <CardTypography props={{title: "Category:", content: product.category}} />
+                                        <CardTypography props={{title: "Room:", content: product.room}} />
+                                        <CardTypography props={{title: "Unit Price:", content: product.unitPrice}} />
+                                        <CardTypography props={{title: "Stocks:", content: product.stocks}} />
+                                        <CardTypography props={{title: "isActive:", content: (product.isActive ? "Yes" : "No")}} />
+                                        <CardTypography props={{title: "isPromoted:", content: (product.isPromoted ? "Yes" : "No")}} />
+                                        <CardTypography props={{title: "No. of Orders:", content: (product.customersOrdered.length > 0 ? product.customersOrdered : 0)}} />
+                                    </Box>
+                                </Grid>
+                        
+                                <Grid item xs={12} sm={12} md={3} lg={3} alignSelf="center">
+                                    <Box 
+                                        sx={{
+                                            display: "flex",
+                                            flexDirection: "column",
+                                            justifyContent: "center",
+                                            alignItems: "center"
+                                        }}
+                                    >
+                                        <CommonButton props={{name: "Edit", color: "#4b5320"}}/>
+                                        {
+                                            product.isPromoted ?
+                                            <CommonButton props={{name: "Undo Promote", color: "#990f02", onClick: () => togglePromote(product)}} />
+                                            :
+                                            <CommonButton props={{name: "Promote", color: "#800080", onClick: () => togglePromote(product)}} />
+                                        }
+                                        {
+                                            product.isActive ?
+                                            <CommonButton props={{name: "Archive", color: "#cc3300", onClick: () => toggleActive(product)}} />
+                                            :
+                                            <CommonButton props={{name: "Unarchive", color: "#4b5320", onClick: () => toggleActive(product)}} />
+                                        }
+                                        <CommonButton props={{name: "Delete", color: "#990f02", onClick: () => deleteProduct(product)}} />
+                                    </Box>
+                                </Grid>
+                            </Grid>
+                        </Card>
                     </Grid>
+                    )
                     
-                    <Grid item xs={12} sm={6} md={6} lg={6}>
-                        <Box
-                            sx={{
-                                display: "flex",
-                                flexDirection: "column",
-                                justifyContent: "center",
-                                alignItems: "center"
-                            }}
-                        >
-                            <CardTypography props={{title: "Product Id:", content: "12312312"}} />
-                            <CardTypography props={{title: "Name:", content: "chikamuna"}} />
-                            <CardTypography props={{title: "Description:", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut luctus arcu. Vestibulum quis malesuada urna. Duis non vulputate dui. In convallis dignissim accumsan. Fusce nec faucibus velit, et malesuada dolor. Suspendisse vehicula urna nibh. Curabitur aliquam, lacus a mollis mattis, turpis mi iaculis ex, ut iaculis leo tortor a nibh. Curabitur et magna pulvinar magna dapibus interdum at sed est. Aliquam ornare gravida fermentum. Sed dignissim volutpat est eu pretium. Duis vestibulum nibh in iaculis vehicula. Nunc pharetra bibendum nunc, eget bibendum elit."}} />
-                            <CardTypography props={{title: "Category:", content: "chika"}} />
-                            <CardTypography props={{title: "Room:", content: "chika"}} />
-                            <CardTypography props={{title: "Unit Price:", content: 1231231}} />
-                            <CardTypography props={{title: "Stocks:", content: 123}} />
-                            <CardTypography props={{title: "isActive:", content: "oo"}} />
-                            <CardTypography props={{title: "isPromoted:", content: "nope"}} />
-                            <CardTypography props={{title: "No. of Orders:", content: "10"}} />
-                        </Box>
-                    </Grid>
-            
-                    <Grid item xs={12} sm={12} md={3} lg={3} alignSelf="center">
-                        <Box 
-                            sx={{
-                                display: "flex",
-                                flexDirection: "column",
-                                justifyContent: "center",
-                                alignItems: "center"
-                            }}
-                        >
-                            <CommonButton props={{name: "Edit", color: "#4b5320"}}/>
-                            <CommonButton props={{name: "Promote", color: "#800080"}} />
-                            <CommonButton props={{name: "Archive", color: "#cc3300"}} />
-                            <CommonButton props={{name: "Delete", color: "#990f02"}} />
-                        </Box>
-                    </Grid>
-                </Grid>
-            </Card>
-        </Grid>
+                })
+            }
+        </>
     )
 }
