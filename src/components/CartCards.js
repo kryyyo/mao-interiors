d2b9b70cba241d749d75eafc46383a8e800fa781
Collene De Silva <collene.desilva@gmail.com>
2022-04-12 01:51:11 +0800

    Map carted products api

--- src/components/CartCards.js
+++ src/components/CartCards.js
@@ -1,71 +1,83 @@
-import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
-import { useState } from "react";
+import { TextField } from "@mui/material";
+import { useState, useEffect } from "react";
 import { Grid } from "@mui/material";
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import { IconButton } from "@mui/material";
 import { Typography, Box } from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
 
 export default function CartCards() {
-    const [quantity, setQuantity] = useState();
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch(`${ process.env.REACT_APP_API_URL }/users/myCart`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            setProducts(data)
+        })
+    });
+
     return (
-        <>
-        <Grid item xs={12} borderBottom={1} pb={5} borderColor="grey.500">
-            <Grid container justifyContent="center" alignItems="flex-start" spacing={3}>
-                <Grid item xs={5} sm={2}>
-                    <FormControl fullWidth>
-                        <InputLabel id="quantity">Qty</InputLabel>
-                        <Select
-                            labelId="quantity"
-                            id="quantity"
-                            value={quantity}
-                            label="Qty"
-                            onChange={(e) => setQuantity(e.target.value)}
-                        >
-                            <MenuItem value={1}>1</MenuItem>
-                            <MenuItem value={2}>2</MenuItem>
-                            <MenuItem value={3}>3</MenuItem>
-                            <MenuItem value={4}>4</MenuItem>
-                            <MenuItem value={5}>5</MenuItem>
-                            <MenuItem value={6}>6</MenuItem>
-                        </Select>
-                    </FormControl>
+        products.map((product) => {
+            return (
+                <Grid item xs={12} borderBottom={1} py={5} borderColor="grey.500">
+                    <Grid container justifyContent="center" alignItems="flex-start" spacing={3}>
+                        <Grid item xs={5} sm={2}>
+                            <IconButton
+                                sx={{ color: "#990f02"}}
+                            >
+                                <DeleteOutlineIcon />
+                            </IconButton>
 
-                    <IconButton
-                        sx={{ color: "#990f02"}}
-                    >
-                        <DeleteOutlineIcon />
-                    </IconButton>
-                </Grid>
+                            <IconButton
+                                sx={{ color: "#4b5320"}}
+                            >
+                                <EditIcon />
+                            </IconButton>
 
-                <Grid item xs={7} sm={5}>
-                    <img src="https://placekitten.com/300/300" alt="mycathover" style={{width: "100%"}}/>
-                </Grid>
-                
-                <Grid item xs={12} sm={5}>
-                    <Grid container justifyContent="center">
-                        <Grid item xs={9} sm={8}>
-                            <Typography variant="h6">product.name</Typography>
-                            <Typography variant="body2">product.category</Typography>
-                            <Typography variant="body1">product.description</Typography>
+                            <TextField
+                                disabled
+                                id="outlined-disabled"
+                                label="Qty"
+                                defaultValue={product.quantity}
+                                sx={{width: "25%"}}
+                            />
                         </Grid>
 
-                        <Grid item xs={3} sm={3}>
-                            <Typography variant="h5"><Box sx={{display: "inline", fontSize: "0.5em"}}>&#8369;</Box><strong>1000</strong></Typography>
+                        <Grid item xs={7} sm={5}>
+                            <img src="https://placekitten.com/300/300" alt="mycathover" style={{width: "100%"}}/>
+                        </Grid>
+                        
+                        <Grid item xs={12} sm={5}>
+                            <Grid container justifyContent="space-between">
+                                <Grid item xs={8} sm={8}>
+                                    <Typography variant="h6">{product.name}</Typography>
+                                    <Typography variant="body2">{product.category}</Typography>
+                                    <Typography variant="body1">{product.description}</Typography>
+                                </Grid>
+
+                                <Grid item xs={4} sm={3}>
+                                    <Typography variant="h5"><Box sx={{display: "inline", fontSize: "0.5em"}}>&#8369;</Box><strong>{product.unitPrice}</strong></Typography>
+                                </Grid>
+                            </Grid>
+
+                            <Grid container justifyContent="space-between" mt={6}>
+                                <Grid item xs={8} sm={8}>
+                                    <Typography variant="h6">Subtotal</Typography>
+                                </Grid>
+
+                                <Grid item xs={4} sm={3}>
+                                    <Typography variant="h4"><Box sx={{display: "inline", fontSize: "0.5em"}}>&#8369;</Box><strong>{product.unitPrice * product.quantity}</strong></Typography>
+                                </Grid>
+                            </Grid>
                         </Grid>
                     </Grid>
-
-                    <Grid container justifyContent="center" mt={6}>
-                        <Grid item xs={8} sm={8}>
-                            <Typography variant="h6">Subtotal</Typography>
-                        </Grid>
-
-                        <Grid item xs={4} sm={3}>
-                            <Typography variant="h4"><Box sx={{display: "inline", fontSize: "0.5em"}}>&#8369;</Box><strong>1000</strong></Typography>
-                        </Grid>
-                    </Grid>
                 </Grid>
-            </Grid>
-        </Grid>
-        </>
+            )
+        })
     )
 }
